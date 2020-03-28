470e2eccedf49043b052d7671891123c4a981a1b
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2020-03-28 17:01:06 +0800

    代码格式化

--- src/Dispatcher/Router.php
+++ src/Dispatcher/Router.php
@@ -2,7 +2,8 @@
 
 namespace Wenprise\Dispatcher;
 
-class Router {
+class Router
+{
 
     /**
      * 功能
@@ -10,41 +11,50 @@ class Router {
      * @param array $url_callbacks
      * @param int   $priority
      */
-    static function routes(array $url_callbacks, $priority = 5) {
+    static function routes(array $url_callbacks, $priority = 5)
+    {
 
         //In form WordPress that these routes ( rewrites ) exist by adding to the rewrite_rules_array
-        add_filter('rewrite_rules_array', function($rules) use ($url_callbacks) {
-            return array_reduce( array_keys($url_callbacks), function($rules, $route) {
-                $newRule = ['^'.trim($route,'/').'/?$' => 'index.php?'.static::query_var_name($route).'=1'];
+        add_filter('rewrite_rules_array', function ($rules) use ($url_callbacks)
+        {
+            return array_reduce(array_keys($url_callbacks), function ($rules, $route)
+            {
+                $newRule = ['^' . trim($route, '/') . '/?$' => 'index.php?' . static::query_var_name($route) . '=1'];
+
                 return $newRule + $rules;
-            }, $rules );
+            }, $rules);
         });
 
-        add_filter('query_vars', function($qvars) use ($url_callbacks) {
-            return array_reduce(array_keys($url_callbacks), function($qvars, $route) {
-                $qvars[] = static::query_var_name($route);
-                return $qvars;
-            },$qvars);
+        add_filter('query_vars', function ($query_vars) use ($url_callbacks)
+        {
+            return array_reduce(array_keys($url_callbacks), function ($query_vars, $route)
+            {
+                $query_vars[] = static::query_var_name($route);
+
+                return $query_vars;
+            }, $query_vars);
         });
 
-        add_action( 'template_redirect', function() use ($url_callbacks) {
+        add_action('template_redirect', function () use ($url_callbacks)
+        {
             global $wp_query;
             foreach ($url_callbacks as $route => $callback) {
-                if ($wp_query->get( static::query_var_name($route)) ) {
+                if ($wp_query->get(static::query_var_name($route))) {
                     $wp_query->is_home = false;
-                    $params = null;
-                    preg_match('#'.trim($route,'/').'#',$_SERVER['REQUEST_URI'],$params);
-                    $res = call_user_func_array($callback,$params);
-                    if($res === false) {
+                    $params            = null;
+                    preg_match('#' . trim($route, '/') . '#', $_SERVER[ 'REQUEST_URI' ], $params);
+                    $res = call_user_func_array($callback, $params);
+                    if ($res === false) {
                         static::send_404();
                     } else {
                         exit();
                     }
                 }
             }
-        }, get_option('wenprise/router-priority',$priority) );
+        }, get_option('wenprise/router-priority', $priority));
 
-        add_action('init', function() use ($url_callbacks) {
+        add_action('init', function () use ($url_callbacks)
+        {
             static::maybe_flush_rewrites($url_callbacks);
         }, 99);
     }
@@ -55,12 +65,13 @@ class Router {
      *
      * @param $url_callbacks
      */
-    static protected function maybe_flush_rewrites($url_callbacks) {
+    protected static function maybe_flush_rewrites($url_callbacks)
+    {
         $current = md5(json_encode(array_keys($url_callbacks)));
-        $cached = get_option(get_called_class(), null );
-        if ( empty( $cached ) ||  $current !== $cached ) {
+        $cached  = get_option(static::class, null);
+        if (empty($cached) || $current !== $cached) {
             flush_rewrite_rules();
-            update_option(get_called_class(), $current );
+            update_option(static::class, $current);
         }
     }
 
@@ -72,19 +83,22 @@ class Router {
      *
      * @return mixed
      */
-    static protected function query_var_name($route) {
+    protected static function query_var_name($route)
+    {
         static $cache;
-        if (!isset($cache[$route])) {
-            $cache[$route] = md5($route);
+        if ( ! isset($cache[ $route ])) {
+            $cache[ $route ] = md5($route);
         }
-        return $cache[$route];
+
+        return $cache[ $route ];
     }
 
 
     /**
      * 发送 404 错误
      */
-    static protected function send_404() {
+    protected static function send_404()
+    {
         global $wp_query;
         status_header('404');
         $wp_query->set_404();
